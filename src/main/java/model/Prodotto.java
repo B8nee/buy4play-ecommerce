207f4dc11b3519691d9d28b52498a3b3bdde1d53
computer <me@computer>
2026-05-31 17:47:14 +0200

    Aggiunto package model e Bean Prodotto

---
+++ src/main/java/model/Prodotto.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Prodotto {
+    private int id;
+    private String nome;
+    private String piattaforma;
+    private double prezzo;
+    private String immagineUrl;
+    
+    public Prodotto() {}
+
+    public Prodotto(int id, String nome, String piattaforma, double prezzo, String immagineUrl) {
+        this.id = id;
+        this.nome = nome;
+        this.piattaforma = piattaforma;
+        this.prezzo = prezzo;
+        this.immagineUrl = immagineUrl;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getPiattaforma() { return piattaforma; }
+    public void setPiattaforma(String piattaforma) { this.piattaforma = piattaforma; }
+
+    public double getPrezzo() { return prezzo; }
+    public void setPrezzo(double prezzo) { this.prezzo = prezzo; }
+
+    public String getImmagineUrl() { return immagineUrl; }
+    public void setImmagineUrl(String immagineUrl) { this.immagineUrl = immagineUrl; }
+}
