d1ce02de328de0dbc87331f53ec3cf2bfade908e
B8nee <danyradut2017@gmail.com>
2026-06-01 23:20:32 +0200

    aggiunto .classpath e .project + configurazione del DataSource, creazione context.xml e modifica di web.xml; creazione del DAO

---
+++ src/main/java/model/ProdottoDAO.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ProdottoDAO {
+    private static DataSource ds;
+
+    static {
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            ds = (DataSource) envCtx.lookup("jdbc/buy4play");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Prodotto> doRetrieveAll() throws SQLException {
+        List<Prodotto> lista = new ArrayList<>();
+        String sql = "SELECT * FROM prodotto";
+        try (Connection conn = ds.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Prodotto p = new Prodotto();
+                p.setId(rs.getInt("id"));
+                p.setNome(rs.getString("nome"));
+                p.setPiattaforma(rs.getString("piattaforma"));
+                p.setPrezzo(rs.getDouble("prezzo"));
+                p.setImmagineUrl(rs.getString("immagine_url"));
+                lista.add(p);
+            }
+        }
+        return lista;
+    }
+}
